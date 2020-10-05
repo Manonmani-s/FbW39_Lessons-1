77263f48679ef1499be38ed2825f6a05d789a798
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2020-10-05 09:12:11 +0200

    Fist Exercise solution uploaded

--- B_Programming-Basics/01-JavaScript_Basic/exercise.js
+++ B_Programming-Basics/01-JavaScript_Basic/exercise.js
@@ -1,5 +1,21 @@
+//Exercise -1-
 //Create an Object named fbw and it has the below props
+const fbw = {};
 //1-Create  a name prop
+fbw.name = 'FbW-39';
 //2-Creat a teacher prop as obj {name, age, course}
-//3-Create  an Array of three participants
-//4-each participant is an obj {name, age, other}
+fbw.teacher = {
+    name: 'Safwan',
+    age: 32,
+    course: 'Web Development'
+};
+
+//3-each participant is an obj {name, age, other}
+let Glauber = {
+    name: 'Glauber',
+    age: '23',
+    other: 'Java Developer'
+};
+
+//4-Create  an Array of three participants
+fbw.participants = [Glauber, 'Adam', 'Arif', 'Tareq', 'Santiago', 'Gabor', 'Nils', 'Nargiza', 'Matheus', 'Daniel', 'Ljiljana', 'Manonmani', 'Ion', 'Rashad'];
