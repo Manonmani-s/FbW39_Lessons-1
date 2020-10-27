7dd4a1244254cf3bdcba4f32c1c824ae55ef3b72
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2020-10-27 16:00:36 +0100

    27 Oct 2020 : assignment

--- B_Programming-Basics/07-Arrays_and_Array_Methods/day-27-10 - 2020/assignments/Sort_and_Filter/index.js
+++ B_Programming-Basics/07-Arrays_and_Array_Methods/day-27-10 - 2020/assignments/Sort_and_Filter/index.js
@@ -47,24 +47,53 @@ const myData = [{
 
 
 //Show Data function :
- function showData(){
-    //  table.classList.add('data');
-    display.innerHTML = ``;
-    for (let index = 0; index < myData.length; index++) {
-        // '+=' : means adding extra elements , while '=': means replace
-        display.innerHTML +=`<tr>
-        <td>${index+1}</td>
-        <td>${myData[index].name}</td>
-        <td>${myData[index].type}</td>
-        <td>${myData[index].rank}</td>
-       </tr>
-       `;
-    }
+
+//showData : defining the tool
+function showData(data , resultDisplay) {
+       
+       resultDisplay.innerHTML = ``;
+       for (let index = 0; index < data.length; index++) {
+          
+           resultDisplay.innerHTML +=`<tr>
+           <td>${index+1}</td>
+           <td>${data[index].name}</td>
+           <td>${data[index].type}</td>
+           <td>${data[index].rank}</td>
+          </tr>
+          `;
+       }
+}
+
+//sortByType()
+function sortByType(data) {
+    
+
+    // take isolated copy keep the refernce safe
+    let sortedByType = data.slice();
+    return sortedByType.sort(function(a , b){
+      //make case insensitive compare
+       if(a.type.toLowerCase() < b.type.toLowerCase()){
+           return -1;
+       }else{
+           return 1;
+       }
+       
+    });
+    
+}
+
+//sortByName() -- please create this tool
+//sortByRank()-- please create this tool
+
+
+function showDataHandler() {
+     // using the tool
+     showData(myData, display);
  }
 
- // * sortByName()
+ // change sortByNameHandler by using sortByName() and showData()
 
- function sortByName(){
+ function sortByNameHandler(){
     // table.classList.remove('data');
      // take isolated copy keep the refernce safe
      let sortedByName = myData.slice();
@@ -95,40 +124,15 @@ const myData = [{
 
  // * sortByType()
 
- function sortByType(){
-    // table.classList.remove('data');
-
-    // take isolated copy keep the refernce safe
-    let sortedByType = myData.slice();
-    sortedByType.sort(function(a , b){
-      //make case insensitive compare
-       if(a.type.toLowerCase() < b.type.toLowerCase()){
-           return -1;
-       }else{
-           return 1;
-       }
-       
-    });
-    display.innerHTML = ``;
-
-
-    for (let index = 0; index < sortedByType.length; index++) {
-       // '+=' : means adding extra elements , while '=': means replace
-       display.innerHTML +=`<tr>
-       <td>${index+1}</td>
-       <td>${sortedByType[index].name}</td>
-       <td>${sortedByType[index].type}</td>
-       <td>${sortedByType[index].rank}</td>
-      </tr>
-      `;
-   }
-    console.table(sortedByType);
+ function sortByTypeHandler(){
+     let sorted = sortByType(myData);
+     showData(sorted, display);
 }
 
 
- // * sortByRank()
+ // change sortByRankHandler by using sortByRank() and showData()
 
- function sortByRank(){
+ function sortByRankHandler(){
     // table.classList.remove('data');
 
     // take isolated copy keep the refernce safe
@@ -154,4 +158,51 @@ const myData = [{
     console.table(sortedByRank);
  }
 
- // create a filter button to take out the type = 'football team' , and the result is sorted by type
+ // create a filter input with a button , have a list of types, to filter according to the input value , and the result is sorted by type and then presented to the filtered table
+
+ //* creating a button called filter - done in index.html
+
+ // * prepare a list of keywords
+// const wordsList = [];
+for (let index = 0; index < myData.length; index++) {
+    // wordsList.push(myData[index].type);
+    keyWords.innerHTML += `<li>${myData[index].type}</li>`;
+    
+}
+
+
+
+// handel the filter action
+
+// a filter function - tool 
+function filterArray(someArr, word) {
+    return someArr.filter(function (item) {
+        
+        if (item.type != word) {
+           
+            return item;
+        }
+    });
+}
+// Handel the click event of filter button
+function dataFilter() {
+    
+    //make a safe copy
+    let filteredArray = myData.slice();
+    
+    // getting the word typed in the input elementt
+    let filterWord = document.getElementById('filter').value;
+   
+    // use a filter tool
+    filteredArray = filterArray(filteredArray, filterWord);
+    
+    // re-using the sort by type tool
+    let sortedArray = sortByType(filteredArray);
+
+     // re-using the show data tool
+    showData(sortedArray, filteredDisplay);
+
+}
+
+
+ 
